94f8add060f7cf6779578c2c72b7cc368f703050
sardar112 <sardarsaqib385@gmail.com>
2022-01-26 00:06:28 +0500

    Header and Footer Components setup

---
+++ src/components/Layouts/header/Header.jsx
@@ -0,0 +1,11 @@
+import './Header.css';
+
+const Header = () => {
+  return (
+    <header>
+      <h2>Feedback Ui App</h2>
+    </header>
+  );
+};
+
+export default Header;
--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import './index.css';
 import App from './App';
+import Header from './components/Layouts/header/Header';
+import Footer from './components/Layouts/footer/Footer';
+
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
+    <Header />
     <App />
+    <Footer />
   </React.StrictMode>,
   document.getElementById('root')
 );
